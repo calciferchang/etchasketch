6ca37c2c51087dc5bb5260669c2c035b1bb55115
Calcifer Chang <142045045+calciferchang@users.noreply.github.com>
2025-07-21 19:44:49 -0400

    replaced let w/ const

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const gridContainer = document.querySelector("#gridContainer");
 const errorMessage = document.querySelector("#errorMessage");
 
 button.addEventListener("click", function newGrid() {
-  let inputValue = document.querySelector("input").value;
+  const inputValue = document.querySelector("input").value;
   document.querySelector("input").value = "";
   errorMessage.textContent = "";
   if (inputValue > 1 && inputValue <= 40) {
@@ -26,8 +26,8 @@ function generateGrid(count) {
     square.style.width = 100 / count + "%";
     square.style.backgroundColor = `rgba(${randomRGB()}, ${randomRGB()}, ${randomRGB()}, 0)`
     square.addEventListener('mouseover', (event) => {
-      let squareColor = event.target.style.backgroundColor.slice(0, -1).split(" ");
-      let newOpacity = Math.min(parseFloat(squareColor[3]) + .1, 1)
+      const squareColor = event.target.style.backgroundColor.slice(0, -1).split(" ");
+      const newOpacity = Math.min(parseFloat(squareColor[3]) + .1, 1)
       squareColor.splice(3, 1, newOpacity + ')')
       event.target.style.backgroundColor = squareColor.join(" ")
       console.log(event.target.style.backgroundColor)
